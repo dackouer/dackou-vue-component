34bc8ce03ea7ca3685875ab7cbeb7ce33a767fe4
dackouer <dackou@163.com>
2023-05-27 16:50:10 +0800

    first commit

--- command/build.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-const { defineConfig,build } = require('vite')
-const vue = require('@vitejs/plugin-vue')
-const vueJsx = require('@vitejs/plugin-vue-jsx')
-
-// package
